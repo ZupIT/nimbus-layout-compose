d1e1fe20b984d56ad6b1cabcca1e67b22f35538a
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-05-26 16:23:43 -0300

    :sparkles: Adds accessibility to touchable
    Signed-off-by: Hernand Azevedo <hernand.azevedo@zup.com.br>

--- layout/src/main/java/br/com/zup/nimbus/compose/layout/ComponentMap.kt
+++ layout/src/main/java/br/com/zup/nimbus/compose/layout/ComponentMap.kt
@@ -1,24 +1,19 @@
+@file:Suppress("UNCHECKED_CAST")
 package br.com.zup.nimbus.compose.layout
 
 import androidx.compose.runtime.Composable
 import br.zup.com.nimbus.compose.ComponentHandler
-import com.zup.nimbus.core.tree.ServerDrivenNode
+import com.fasterxml.jackson.core.type.TypeReference
 
 val layoutComponents: Map<String, @Composable ComponentHandler> = mapOf(
     "layout:touchable" to @Composable { element, children ->
         Touchable(
             onPress = element.properties!!["onPress"] as (Any?) -> Unit,
             children = children,
-            accessibility = getAccessibility(element)
+            accessibility = if (element.properties!!.containsKey("accessibility")) {
+                (element.properties!!["accessibility"] as Map<String, Any?>)
+                    .parse(object : TypeReference<Accessibility>() {})
+            } else null
         )
     },
 )
-
-private fun getAccessibility(element: ServerDrivenNode): Accessibility? =
-    if (element.properties!!.containsKey("accessibility")) {
-        val accessibility = element.properties!!["accessibility"] as Map<*, *>
-        Accessibility(
-            label = accessibility["label"] as String?,
-            isHeader = accessibility["isHeader"] as Boolean?
-        )
-    } else null

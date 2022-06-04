21fc08ddbefa20adb60446b4cb2fe1f30e85274f
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-06-04 10:54:24 -0300

    :sparkles: ADd runner deps
    Signed-off-by: Hernand Azevedo <hernand.azevedo@zup.com.br>

--- layout/build.gradle.kts
+++ layout/build.gradle.kts
@@ -23,9 +23,9 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.4.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.4")
     androidTestImplementation ("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.3.0")
-    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.3.0")
-
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.4.0")
+    androidTestImplementation("androidx.test:runner:1.4.0")
     androidTestImplementation ("androidx.compose.ui:ui-test:${rootProject.extra["compose_version"]}")
 
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")

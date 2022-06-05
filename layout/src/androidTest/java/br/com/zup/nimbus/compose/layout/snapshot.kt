c8768598dc3ac85dedccdbf295a57dbc09d7f6d3
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-06-05 12:06:12 -0300

    :white_check_mark: wait before compare screenshots
    Signed-off-by: Hernand Azevedo <hernand.azevedo@zup.com.br>

--- layout/src/androidTest/java/br/com/zup/nimbus/compose/layout/snapshot.kt
+++ layout/src/androidTest/java/br/com/zup/nimbus/compose/layout/snapshot.kt
@@ -79,7 +79,7 @@ fun ScreenshotTest.executeScreenshotTest(jsonFile: String, composeTestRule: Comp
         ScreenTest(getJson(jsonFile) ?: "")
     }
     composeTestRule.waitUntilDoesNotExist(hasTestTag(loadingTag))
-    Thread.sleep(50)
+    Thread.sleep(100)
     compareScreenshot(composeTestRule)
 }
 

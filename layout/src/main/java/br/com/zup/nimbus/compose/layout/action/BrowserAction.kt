7f3dcc8af1c54837e0010ac2ec5b852cdddb306f
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-06-23 12:33:44 -0300

    Adds open url action
    Signed-off-by: Hernand Azevedo <hernand.azevedo@zup.com.br>

--- layout/src/main/java/br/com/zup/nimbus/compose/layout/action/BrowserAction.kt
+++ layout/src/main/java/br/com/zup/nimbus/compose/layout/action/BrowserAction.kt
@@ -5,14 +5,14 @@ import android.net.Uri
 import android.util.Log
 import androidx.core.content.ContextCompat
 import br.com.zup.nimbus.compose.layout.ACTION_LOG_TAG
-import br.zup.com.nimbus.compose.NimbusTheme
+import br.zup.com.nimbus.compose.NimbusTheme.nimbusStaticState
 import com.zup.nimbus.core.render.ActionEvent
 
 internal fun browserAction(event: ActionEvent) {
     try {
         val url = event.action.properties!!["url"] as String?
         val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        NimbusTheme.nimbusStaticState?.let {
+        nimbusStaticState?.let {
             ContextCompat.startActivity(it.applicationContext, browserIntent, null)
         }
     } catch (e: Throwable) {

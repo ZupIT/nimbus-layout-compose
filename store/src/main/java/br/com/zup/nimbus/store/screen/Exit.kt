5ef7c6ee3f133e6e824dcfccf883f0b4dfaffc5d
Tiago Peres França <tiago.franca@zup.com.br>
2022-12-13 10:21:55 -0300

    fixes tests + self review

--- store/src/main/java/br/com/zup/nimbus/store/screen/Exit.kt
+++ store/src/main/java/br/com/zup/nimbus/store/screen/Exit.kt
@@ -3,11 +3,8 @@ package br.com.zup.nimbus.store.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
@@ -17,17 +14,5 @@ fun ExitScreen() {
         .fillMaxSize()
         .wrapContentSize(Alignment.Center)) {
         Text("Exit")
-        Counter()
-    }
-}
-
-@Composable
-fun Counter() {
-    val (count, setCount) = remember {
-        mutableStateOf(0)
-    }
-
-    Button(onClick = { setCount(count + 1) }) {
-        Text(count.toString())
     }
 }
